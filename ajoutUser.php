0b226a8b2f61f1bd17ce480ec167e326a07e9b8a
Herbert <hainautandy@gmail.com>
2025-10-15 14:39:08 +0200

    Ajout consigne fonction ajoutUser.php

--- ajoutUser.php
+++ ajoutUser.php
@@ -2,7 +2,7 @@
 
 $stmt = $db->prepare("insert into utilisateur values(:nom,:prenom,:mail,:mdp)");
 
-
+//Quand le mec appuie sur "s'inscrire", les infos sont recup et envoye dans le execute qui enverra les donnees dans la base
 
 
 $stmt->execute(array(:nom'=>,null':prenom'=>null,':mail'=>null,':mdp'=>null));
