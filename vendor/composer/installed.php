0a1905ba66664590bbda8c5680fa02bfc8bbb0af
Andy Hainaut <126461343+Andy80130@users.noreply.github.com>
2025-12-09 09:39:24 +0100

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'haina/sae3_20252026',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '11e30ede1f414eabeb0a63dc2ad918a22423cf7a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'haina/sae3_20252026' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '11e30ede1f414eabeb0a63dc2ad918a22423cf7a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.1',
+            'version' => '7.0.1.0',
+            'reference' => '360ae911ce62e25e11249f6140fa58939f556ebe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
