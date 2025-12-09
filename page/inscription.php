44f743e66f0e3ed984a41a6b17cdb66a7b47e785
Herbert <hainautandy@gmail.com>
2025-12-09 12:54:09 +0100

    Correction des messages d'erreurs

--- page/inscription.php
+++ page/inscription.php
@@ -9,8 +9,7 @@ $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
+use PHPMailer\PHPMailer\Exception as PHPMailerException;
 $errors = [];
 
 // Traitement de la requête POST
@@ -88,11 +87,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         ';
                         $mail->send();
                         echo 'Message envoyé avec succès !';
-                    } catch (Exception $e) {
+                    } catch (PHPMailerException $e) {
                         $errors[] = "Erreur lors de l'envoi" . $mail->ErrorInfo;
                     }
-                }else {
-                    $errors[] = "Erreur rencontré lors de la récupération des données";
                 }
 
                 if (isset($_SESSION['user_id']) || isset($_SESSION['mail'])) {
@@ -121,78 +118,77 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../css/styleCompte.css">
 </head>
 <body>
-    <main class="container container-infos">
-        <header class="titre">
-            <h2>Inscription à StudyGo</h2>
-            <div class="text-center">
-                L'appli de co-voiturage dans la région ammiennoise pour les étudiants de l'IUT UPJV que vous allez adorer !
-            </div>
-            <div class="text-center">
-                Vous êtes nouveau ? Alors n'hésitez pas à vous inscrire, c'est rapide et 100% gratuit !
-            </div>
-        </header>
-        <?php
-        if (!empty($errors)) {
-            foreach ($errors as $error) {
-                echo "<p style='color:red; text-align: center;'>$error</p>";
-            }
+<main class="container container-infos">
+    <header class="titre">
+        <h2>Inscription à StudyGo</h2>
+        <div class="text-center">
+            L'appli de co-voiturage dans la région ammiennoise pour les étudiants de l'IUT UPJV que vous allez adorer !
+        </div>
+        <div class="text-center">
+            Vous êtes nouveau ? Alors n'hésitez pas à vous inscrire, c'est rapide et 100% gratuit !
+        </div>
+    </header>
+    <?php
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p style='color:red; text-align: center;'>$error</p>";
         }
-        ?>
-        <section class="shadow p-4 rounded saisie-infos">
+    }
+    ?>
+    <section class="shadow p-4 rounded saisie-infos">
 
-            <form method="POST" action="">
-                <div class="row">
-                    <div class="col">
-                        <label for="nom">Nom</label>
-                        <input type="text" id="nom" name="nom" required />
-                    </div>
-                    <div class="col">
-                        <label for="prenom">Prénom</label>
-                        <input type="text" id="prenom" name="prenom" required />
-                    </div>
+        <form method="POST" action="">
+            <div class="row">
+                <div class="col">
+                    <label for="nom">Nom</label>
+                    <input type="text" id="nom" name="nom" required />
                 </div>
-
-                <div class="row">
-                    <div class="col">
-                        <label for="email">Email</label>
-                        <input type="email" id="email" name="email" required />
-                    </div>
-                    <div class="col">
-                        <label for="telephone">Téléphone</label>
-                        <input type="text" id="telephone" name="telephone" placeholder="0699994810" required />
-                    </div>
+                <div class="col">
+                    <label for="prenom">Prénom</label>
+                    <input type="text" id="prenom" name="prenom" required />
                 </div>
-
-                <div class="row">
-                    <div class="col">
-                        <label for="password">Mot de passe</label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    <div class="col">
-                        <label for="verifPassword">Confirmer votre mot de passe</label>
-                        <input type="password" id="verifPassword" name="verifPassword" required />
-                    </div>
-                </div>
-
-                <button type="submit" name="submit">Créer le compte</button>
-            </form>
-
-            <div class="text-center">
-                <p class="fs-6 form-text">Vous avez déjà un compte ?
-                    <a href="connexion.php" class="text-decoration-none">Se connecter</a>
-                </p>
             </div>
 
-            <div class="text-center">
-                <p class="fs-6 form-text">
-                    En créant un compte, vous acceptez nos conditions d’utilisation qui garantissent une expérience
-                    respectueuse et responsable pour tous. Nous vous invitons à consulter nos règles de sécurité,
-                    nos politiques d’annulation et notre charte de bonne conduite afin de profiter du service en toute confiance.
-                </p>
+            <div class="row">
+                <div class="col">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div class="col">
+                    <label for="telephone">Téléphone</label>
+                    <input type="text" id="telephone" name="telephone" placeholder="0699994810" required />
+                </div>
             </div>
 
-        </section>
-    </main>
+            <div class="row">
+                <div class="col">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div class="col">
+                    <label for="verifPassword">Confirmer votre mot de passe</label>
+                    <input type="password" id="verifPassword" name="verifPassword" required />
+                </div>
+            </div>
+
+            <button type="submit" name="submit">Créer le compte</button>
+        </form>
+
+        <div class="text-center">
+            <p class="fs-6 form-text">Vous avez déjà un compte ?
+                <a href="connexion.php" class="text-decoration-none">Se connecter</a>
+            </p>
+        </div>
+
+        <div class="text-center">
+            <p class="fs-6 form-text">
+                En créant un compte, vous acceptez nos conditions d’utilisation qui garantissent une expérience
+                respectueuse et responsable pour tous. Nous vous invitons à consulter nos règles de sécurité,
+                nos politiques d’annulation et notre charte de bonne conduite afin de profiter du service en toute confiance.
+            </p>
+        </div>
+
+    </section>
+</main>
 </body>
-
 </html>
